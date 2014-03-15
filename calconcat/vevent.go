30eaa99c0c6222eaacfc2089892fb64e8e3ecadb
Ellie Frost <web@stillinbeta.com>
2014-03-15 12:03:34 -0400

    Finished up vevent reader

---
+++ calconcat/vevent.go
@@ -0,0 +1,55 @@
+package calconcat
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "strings"
+)
+
+const (
+    vevent_probable_size = 20
+    vevent_start = "BEGIN:VEVENT" // RFC 5545 ;)
+    vevent_end = "END:VEVENT"
+)
+
+type NoMoreVevents struct{}
+
+func (_ NoMoreVevents) Error() string {
+    return "That's all the vevents we got"
+}
+
+type Vevent struct {
+    err error
+    vevent string
+}
+
+func GetVevents (stream io.Reader, c chan Vevent) {
+    in_vevent := false
+    buf := make([]string, 0, vevent_probable_size)
+    scanner := bufio.NewScanner(stream)
+    for scanner.Scan() {
+        line := scanner.Text()
+        if in_vevent {
+            buf = append(buf, line)
+            if line == vevent_end {
+                c <- Vevent{nil, strings.Join(buf, "\r\n")}
+
+                // Start all over again
+                buf = make([]string, 0, vevent_probable_size)
+                in_vevent = false
+            }
+        } else if line == vevent_start {
+            in_vevent = true
+            buf = append(buf, line)
+        }
+    }
+
+    if err := scanner.Err(); err != nil {
+        fmt.Fprintln(os.Stderr, "reading standard input:", err)
+        c <- Vevent{NoMoreVevents{}, ""}
+    }
+    c <- Vevent{NoMoreVevents{}, ""}
+}
+
---
+++ calconcat/vevent_test.go
@@ -0,0 +1,67 @@
+package calconcat
+
+import (
+    "strings"
+    "testing"
+    "os"
+    "time"
+)
+
+//import "calconcat/vevent"
+
+const ical_file = "../example.ical"
+
+func checkIsVevent(s string) bool {
+    return (strings.HasPrefix(s, "BEGIN:VEVENT") &&
+            strings.HasSuffix(s, "END:VEVENT"))
+}
+
+func Test_GetVevents(t *testing.T) {
+
+    timeout := make(chan bool, 1)
+    go func() {
+            time.Sleep(1 * time.Second)
+            timeout <- true
+    }()
+
+    file, err := os.Open(ical_file)
+    if err != nil {
+        t.Fatalf("Couldn't open ical file: %v", err)
+    }
+
+    channel := make(chan Vevent, 1)
+
+    go GetVevents(file, channel)
+
+    vevent := <-channel
+    if vevent.err != nil {
+        t.Fatalf("First vevent had an error")
+    }
+    if strings.Index(vevent.vevent, "Bastille") == -1 {
+        t.Fatalf("First event isnt' Bastille day (got %v)", vevent.vevent)
+    }
+    if !checkIsVevent(vevent.vevent) {
+        t.Fatalf("First event isnt' a VEVENT")
+    }
+
+    vevent = <-channel
+    if vevent.err != nil {
+        t.Fatalf("Second vevent had an error")
+    }
+    if strings.Index(vevent.vevent, "Networld+Interop") == -1 {
+        t.Fatalf("Second event isn't Networld+Interop conference")
+    }
+    if !checkIsVevent(vevent.vevent) {
+        t.Fatalf("Second event isnt' a VEVENT")
+    }
+
+    select {
+    case vevent = <-channel :
+        if vevent.err == nil {
+            t.Fatalf("Third vevent didn't error")
+        }
+
+    case <-timeout:
+        t.Fatalf("Timout waiting for last element")
+    }
+}
