b99bd9f5adcfe79f4e136548d4fdefd7e6f6ec3a
Ellie Frost <web@stillinbeta.com>
2014-07-13 21:51:18 -0700

    Vevent streaming

--- calconcat/vevent.go
+++ calconcat/vevent.go
@@ -12,6 +12,8 @@ const (
     vevent_probable_size = 20
     vevent_start = "BEGIN:VEVENT" // RFC 5545 ;)
     vevent_end = "END:VEVENT"
+    vtimezone_start = "BEGIN:VTIMEZONE"
+    vtimezone_end = "END:VTIMEZONE"
 )
 
 type NoMoreVevents struct{}
@@ -25,25 +27,46 @@ type Vevent struct {
     Vevent string
 }
 
-func GetVevents (stream io.Reader, c chan Vevent) {
+type Vtimezone {
+    TZID string
+    VTimezone string
+}
+
+func GetVevents (stream io.Reader, vevents chan Vevent, vtimezones chan Vtimezone) {
     in_vevent := false
+    in_vtimezone := false
+    tzid := ""
     buf := make([]string, 0, vevent_probable_size)
     scanner := bufio.NewScanner(stream)
+
+    // Sax-y Vevent parser
     for scanner.Scan() {
         line := scanner.Text()
         if in_vevent {
             buf = append(buf, line)
             if line == vevent_end {
-                c <- Vevent{nil, strings.Join(buf, "\r\n")}
+                vevents <- Vevent{nil, strings.Join(buf, "\r\n")}
 
                 // Start all over again
                 buf = make([]string, 0, vevent_probable_size)
                 in_vevent = false
             }
+        } else if in_vtimezone {
+            buf = append(buf, line)
+            if line == vtimezone_end {
+                vtimezones <- Vtimezone{tzid, strings.Join(buf, "\r\n")}
+
+                tzid := ""
+                buf = make([]string, 0, vevent_probable_size)
+                in_vtimezone = false
         } else if line == vevent_start {
             in_vevent = true
             buf = append(buf, line)
+        } else if line == vtimezone_start {
+            in_vtimezone = true
+            buf = append(buf, line)
         }
+
     }
 
     if err := scanner.Err(); err != nil {
